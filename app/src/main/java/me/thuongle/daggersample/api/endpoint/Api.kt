6f2acb5af2c131164e4a8e7dba32def969033916
Thuong Le <thuongle.it@gmail.com>
2017-10-10 18:23:16 +0300

    First commit of draft

--- app/src/main/java/me/thuongle/daggersample/api/endpoint/Api.kt
+++ app/src/main/java/me/thuongle/daggersample/api/endpoint/Api.kt
@@ -4,6 +4,7 @@ import io.reactivex.Flowable
 import me.thuongle.daggersample.api.model.Item
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.util.*
 
 val BASE_URL = "https://hacker-news.firebaseio.com/v0/"
 
--- app/src/main/java/me/thuongle/daggersample/api/model/Item.kt
+++ app/src/main/java/me/thuongle/daggersample/api/model/Item.kt
@@ -1,6 +1,7 @@
 package me.thuongle.daggersample.api.model
 
 import com.google.gson.annotations.SerializedName
+import java.util.*
 
 data class Item(
         val id: Long,
