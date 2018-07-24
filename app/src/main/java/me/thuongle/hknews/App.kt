731be08998db81ab486383ee00c1d37e915fcbfe
Thuong Le <thuongle.it@gmail.com>
2018-07-24 22:18:49 +0300

    Integrate Timber log with Crashlytics

--- app/src/main/java/me/thuongle/hknews/App.kt
+++ app/src/main/java/me/thuongle/hknews/App.kt
@@ -2,8 +2,12 @@ package me.thuongle.hknews
 
 import android.app.Activity
 import android.app.Application
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import io.fabric.sdk.android.Fabric
 import me.thuongle.hknews.di.AppInjector
 import timber.log.Timber
 import javax.inject.Inject
@@ -14,12 +18,44 @@ open class App : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+
+        val crashlytics = CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build()
+        Fabric.with(this,  Crashlytics.Builder().core(crashlytics).build())
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        Timber.plant(CrashlyticsTree())
+
         AppInjector.init(this)
     }
 
     override fun activityInjector() = dispatchingAndroidInjector
+
+    inner class CrashlyticsTree : Timber.Tree() {
+        private val KEY_PRIORITY = "priority"
+        private val KEY_TAG = "tag"
+        private val KEY_MESSAGE = "message"
+
+        override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
+            when (priority) {
+                Log.VERBOSE, Log.DEBUG, Log.INFO -> return
+
+                else -> {
+                    Crashlytics.setInt(KEY_PRIORITY, priority)
+                    Crashlytics.setString(KEY_TAG, tag)
+                    Crashlytics.setString(KEY_MESSAGE, message)
+
+                    if (t == null) {
+                        Crashlytics.logException(Exception(message))
+                    } else {
+                        Crashlytics.logException(t)
+                    }
+                }
+            }
+        }
+    }
 }
 
